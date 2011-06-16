3a639301a66b51e308c15beb01abc2a9cadddf4f
Stuart Johnson <xenoskin@gmail.com>
2011-06-16 15:08:47 -0600

    Finished first cut of the User model eg chapter 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
+  resources :users
+  
   match '/signup',  :to => 'users#new'
-
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   
   root :to => 'pages#home'
   
+  
+  #get "users/new" might need this
   get "pages/home"
   get "pages/contact"
   get "pages/about"
